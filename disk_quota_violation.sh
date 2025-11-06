84512fde576880bfe2fb58b0c301f9731d47def6
Lokdash <faunaut@gmail.com>
2025-11-06 21:54:31 +0300

    Update disk_quota_violation.sh

--- disk_quota_violation.sh
+++ disk_quota_violation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-VIOLFOUND=0
-MAXDISKUSAGE=20000 #в Мб
+VIOLFOUND=0 #установка переменной для подсчета нарушений
+MAXDISKUSAGE=20000 #установка переменной для лимита в Мб
 
+#проверка всех системных пользователей с UID выше 100
 for name in $(cut -d: -f1,3 /etc/passwd | awk -F: '$2 > 99 {print $1}')
 do
     /bin/echo -n "User $name exceeds disk quota. Disk usage is: "
@@ -11,6 +12,7 @@ do
 
 done | awk "\$9 > $MAXDISKUSAGE {print \$0; found=1} END {exit !found}" && VIOLFOUND=1
 
+#вывод в том случае, если квота соблюдена
 if [ $VIOLFOUND -eq 0 ]; then
         echo "No quota violations found"
 fi
